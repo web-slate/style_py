ac11fa41698531131717ac9dd682d4edf26898ec
Venkat.R <ramsunvtech@gmail.com>
2024-05-02 00:22:33 +0800

    rename to module name

--- utils/blockquote.py
+++
@@ -1,5 +0,0 @@
-def blockquote(items, indent_char='> ', emoji='💬', new_line = False, returnValue = False):
-    if (new_line):
-        print('\n')
-    for item in items:
-        print(f"{emoji} {indent_char}{item}")
