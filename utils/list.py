39a70fd9e789ca26c148e19cbbeff573725b1a4a
Venkat.R <ramsunvtech@gmail.com>
2024-05-02 00:22:34 +0800

    rename to module name

--- utils/list.py
+++
@@ -1,7 +0,0 @@
-def ordered_list(items, returnValue = False):
-    for i, item in enumerate(items, start=1):
-        print(f"{i}. {item}")
-
-def bullet_list(items, bullet_char='*', returnValue = False):
-    for item in items:
-        print(f"{bullet_char} {item}")
