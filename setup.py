a3a1a7729e81997cabc8cf7a1d3d458246d7b2fd
Venkat.R <ramsunvtech@gmail.com>
2024-05-01 22:23:06 +0800

    update github link

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stylepy',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[],
     author='Venkatraman.R',
@@ -10,7 +10,7 @@ setup(
     description='Format CLI output like HTML.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/web-slate/style_py',
+    url='https://github.com/web-slate/stylepy',
     license='MIT',
     python_requires=">=3.3",
 )
