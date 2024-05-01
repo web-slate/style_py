6279970e4effe8e185b087e56e656c93c04992fd
Venkat.R <ramsunvtech@gmail.com>
2024-05-02 00:22:33 +0800

    remove

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-text-formatter
--------------------------------------------
-A pretty formatter for Python strings.
-"""
-
-__author__ = "Venkatraman.R (ramsunvtech@gmail.com) @ramsunvtech"
-__version__ = "0.2.0"
-__license__ = "MIT"
