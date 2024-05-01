31fbaa947b1e3023d639b6f2ce78cd97ed82b745
Venkat.R <ramsunvtech@gmail.com>
2024-05-01 23:46:38 +0800

    add test file

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,11 +1,28 @@
 import pytest
 
-from stylepy import h1, h2, h3, h4, h5, h6
+from stylepy import h1, h2
 
 def test_h1():
-    assert h1("Hello") == "<h1>Hello</h1>", "h1 should wrap text in <h1> tags"
+    expected = """
+========================================
+Test Heading1
+========================================"""
+    
+    # Call the function and capture its output
+    actual = h1("Test Heading1", returnValue=True)
+    
+    # Assert that the function returns the expected value
+    assert actual == expected
+
 
 def test_h2():
-    assert h2("World") == "<h2>World</h2>", "h2 should wrap text in <h2> tags"
-
-# Add more tests for h3, h4, h5, h6
+    expected = """
+-----------------------------------
+Test Heading2
+-----------------------------------"""
+    
+    # Call the function and capture its output
+    actual = h2("Test Heading2", returnValue=True)
+    
+    # Assert that the function returns the expected value
+    assert actual == expected
