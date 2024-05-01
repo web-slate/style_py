43cbf5c9bac0d0c09d208f9a63affbc627693aaa
Venkat.R <ramsunvtech@gmail.com>
2024-05-02 00:22:33 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stylepy',
-    version='0.3',
+    version='0.8',
     packages=find_packages(),
     install_requires=[],
     author='Venkatraman.R',
