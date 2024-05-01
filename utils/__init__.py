4cddc6a01785e08af03c0d6a6b257deecb86e6bd
Venkat.R <ramsunvtech@gmail.com>
2024-05-01 22:23:06 +0800

    add root init

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+text-formatter
+-------------------------------------------
+A pretty formatter for Python strings.
+"""
+
+__author__ = "Venkatraman.R (ramsunvtech@gmail.com) @ramsunvtech"
+__version__ = "0.2.0"
+__license__ = "MIT"
